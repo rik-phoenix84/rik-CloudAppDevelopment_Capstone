5ca8f4754f3f58855481f46a6e3a11f764509040
rik-phoenix84 <riccardo.abballe@gmail.com>
2023-11-17 09:14:47 -0500

    fix urls 2

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -156,7 +156,7 @@ def add_review(request, dealer_id):
 
             new_payload = {}
             new_payload["review"] = payload
-            review_post_url = "https://riccardoabba-5000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/post_review"
+            review_post_url = "https://riccardoabba-5000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/post_review"
             post_request(review_post_url, new_payload, id=id)
         return redirect("djangoapp:dealer_details", id=id)
 
